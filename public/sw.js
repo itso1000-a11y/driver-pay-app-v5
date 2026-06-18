ec6624353395734e48f0ef0927dd689289950926
itso1000-a11y <itso1000@gmail.com>
2026-06-18 19:45:08 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "driver-pay-v5-icon-paths-cache-v5-0-0";
+const CACHE_NAME = "driver-pay-v5-clean-zoom-empty-display-cache-v5-0-0";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon-v5-192.png", "/icons/icon-v5-512.png", "/favicon.ico", "/apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
